69323a4899f428f716b1731a7636a0d2d829110f
Austin Gilbert <ceretullis@gmail.com>
2014-02-01 12:22:27 -0600

    Implemented Copy/Move constructors and assignment operators for DataCacheOid, though I'm not fully convinced yet of the current semantics, as I think it will potentially lead to a lot of fragmentation of the DataCache. Creating a new oid really means we want a new entry in the DataCache.
    TODO: Perhaps we will need a pool of discarded DataCache
    objects that we can reuse.

--- DataCache/DataCacheOid.hpp
+++ DataCache/DataCacheOid.hpp
@@ -7,26 +7,33 @@ namespace DataCache { namespace Testing {
 }}
 
 namespace DataCache {
-
-    // A tag type indicating that we should increment the type when
-    // any object is created.
-    struct CountAllObjects {};
     
     // Object Id base class.
     //
     // Every object stored in the data cache needs
     // to inherit from DataCacheOid. All data cache
     // entries are referenced using the object's <oid_>
-    class DataCacheOid : private ObjectCounter<CountAllObjects>
+    //
+    // The copy constructor and assignment operators create
+    // new OIDs. The move constructor and assignment operators
+    // use the same oid as the moved object, assuming ownership
+    // of the DataCache locations referenced by this OID.
+    class DataCacheOid : private ObjectCounter<Details::CountAllObjects>
     {
     public:
         DataCacheOid();
+        
+        DataCacheOid(const DataCacheOid& cache);
+        DataCacheOid(DataCacheOid&& cache);
+        
+        DataCacheOid& operator=(const DataCacheOid& cache);
+        DataCacheOid& operator=(DataCacheOid&&);
         
         inline std::size_t oid(void) const { return oid_; }
         
     private:
         friend class Testing::DataCacheOidResetter;
-        
+
     private:
         std::size_t oid_;
     };
--- DataCache/Source/DataCacheOid.cpp
+++ DataCache/Source/DataCacheOid.cpp
@@ -2,9 +2,42 @@
 
 namespace DataCache {
 
+    namespace {
+        // perform the oid offset that we would like to use
+        // in the DataCache. In this case, we would like
+        // OIDs to start at 0.
+        inline std::size_t offset_oid(std::size_t value)
+        {
+            return value - 1;
+        }
+    }
+    
     DataCacheOid::DataCacheOid()
-        : oid_(Count() - 1)
+        : oid_(offset_oid(count()))
     {
+    }
+
+    DataCacheOid::DataCacheOid(const DataCacheOid& cache)
+        : oid_(offset_oid(count()))
+    {
+    }
+    
+    DataCacheOid::DataCacheOid(DataCacheOid&& cache)
+        : ObjectCounter<Details::CountAllObjects>(Details::NoIncrement())
+        , oid_(cache.oid_)
+    {
+    }
+    
+    DataCacheOid& DataCacheOid::operator=(const DataCacheOid& cache)
+    {
+        oid_ = offset_oid(count());
+        return *this;
+    }
+    
+    DataCacheOid& DataCacheOid::operator=(DataCacheOid&& cache)
+    {
+        oid_ = std::move(cache.oid_);
+        return *this;
     }
 
 }

46538cba4e7b810d938b58ffdead9fca83adb62e
Austin Gilbert <ceretullis@gmail.com>
2014-02-02 14:20:25 -0600

    A helper template used to invoke BaseType::register_field. Intended to be used as a function local static variable to ensure that field registration occurs once and only once.

---
+++ DataCache/Details/FieldRegistrationLatch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace DataCache { namespace Details {
+    
+    // A helper to invoke BaseType::register_field.
+    //
+    // It is intended to be used in conjection with a function local
+    // static to provide a latch (ensure we register fields only once).
+    template<class BaseType, class FieldType>
+    class FieldRegistrationLatch
+    {
+    public:
+        FieldRegistrationLatch(const std::size_t fieldId)
+        {
+            BaseType::template register_field<BaseType, FieldType>(fieldId);
+        }
+    };
+}}

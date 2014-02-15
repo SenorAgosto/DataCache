1289206f0d93c911e497bb034419dc4bc7e86716
Austin Gilbert <ceretullis@gmail.com>
2014-02-14 22:11:38 -0600

    Moved DataCacheAccessor implementation to .cpp. Removed DataCache/Testing/Dummy.cpp. Implemented DataCacheAccessor::clear() to clear the data cache.

--- DataCache/Testing/DataCacheAccessor.hpp
+++ DataCache/Testing/DataCacheAccessor.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
+namespace DataCache {
+    class DataCache;
+}
+
 namespace DataCache { namespace Testing {
 
     // A test helper for getting access to private data on the <DataCache>
     class DataCacheAccessor
     {
     public:
-        DataCacheAccessor(DataCache& cache)
-            : cache_(cache)
-        {
-        }
+        DataCacheAccessor(DataCache& cache);
+        void clear(void);
 
     private:
         DataCache& cache_;

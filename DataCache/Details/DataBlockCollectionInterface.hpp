31d23827373ebc8497e94d53842e8be118f4da96
Austin Gilbert <ceretullis@gmail.com>
2014-03-24 22:33:57 -0500

    Added create_object to DataBlockCollectionInterface. Added initial implementation of create_object in DataBlockCollection.

--- DataCache/Details/DataBlockCollectionInterface.hpp
+++ DataCache/Details/DataBlockCollectionInterface.hpp
@@ -1,14 +1,16 @@
 #pragma once
+#include <cstddef>
 
 namespace DataCache { namespace Details {
   
-    // A base class for type erasure that defines no interface.
+    // A base class for type erasure that defines minimal interface.
     //
     // Derived classes will be cast to their concrete types prior to
-    // use.
+    // use, other than calling create_object()
     class DataBlockCollectionInterface
     {
     public:
         virtual ~DataBlockCollectionInterface() = 0;
+        virtual void create_object(const std::size_t oid) = 0;
     };
 }}

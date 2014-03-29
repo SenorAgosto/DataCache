d72d11e295873ae4a2ad92441bd7b09298c32705
Austin Gilbert <ceretullis@gmail.com>
2014-03-28 21:27:29 -0500

    Hiding build directories because it is annoying to see them in my working directory.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mkdir -p build_xcode && cd build_xcode && cmake -G "Xcode" .. && open DataCache.xcodeproj && cd ..
+mkdir -p .build_xcode && cd .build_xcode && cmake -G "Xcode" .. && open DataCache.xcodeproj && cd ..

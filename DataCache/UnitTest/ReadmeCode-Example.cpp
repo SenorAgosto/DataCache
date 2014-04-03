66bb20e4fcf8fec2ecece2f484a53f96dd4bb9ab
Austin Gilbert <ceretullis@gmail.com>
2014-04-02 21:38:09 -0500

    Making example code from Readme a unit test, so I know it is valid.

--- DataCache/UnitTest/ReadmeCode-Example.cpp
+++ DataCache/UnitTest/ReadmeCode-Example.cpp
@@ -18,14 +18,26 @@ namespace {
     };
     
     // Declare a type using the DataCache
-    struct MyObject
-    : public DataCache::UsingDataCacheIn<MyObject>
-    , public DataCache::DataBlock<MyObject, Fields::field_1, Fields::field_1_type>
-    , public DataCache::DataBlock<MyObject, Fields::field_2, Fields::field_2_type>
+    class MyObject
+        : public  DataCache::UsingDataCacheIn<MyObject>
+        , private DataCache::DataBlock<MyObject, Fields::field_1, Fields::field_1_type>
+        , private DataCache::DataBlock<MyObject, Fields::field_2, Fields::field_2_type>
     {
+    public:
         void Foo(void);
     };
 
+    void MyObject::Foo(void)
+    {
+		// the only inconvenience is we have to use our OID to
+		// get the location of our fields we want to modify here.
+		auto& field1 = get<Fields::field_1_type>(oid(), Fields::field_1);
+		auto& field2 = get<Fields::field_2_type>(oid(), Fields::field_2);
+        
+		field1 += field2;
+		field2 /= 2;
+    }
+    
     TEST(verifyReadmeExampleCodeCompiles)
     {
         MyObject object;
